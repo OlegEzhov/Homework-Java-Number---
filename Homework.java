9b5a98bfbb9607345cec53266b96a97e260a92dd
OlegEzhov <127249557+OlegEzhov@users.noreply.github.com>
2023-03-26 11:12:09 +0200

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,70 @@
+public class Homework {
+    public static void main(String[] args) {
+        int q =637;
+        // перевод 637 из 10 -> 2
+        System.out.println(637 / 2); // 318
+        System.out.println(637 % 2);        //1
+
+        System.out.println(318 / 2 ); //159
+        System.out.println(318 % 2);        //0
+
+        System.out.println(159 / 2); //79
+        System.out.println(159 % 2);        //1
+
+        System.out.println(79 / 2); //39
+        System.out.println(79 % 2);         //1
+
+        System.out.println(39 / 2); //19
+        System.out.println(39 % 2);         //1
+
+        System.out.println(19 / 2); //9
+        System.out.println(19 % 2);         //1
+
+        System.out.println(9 / 2); //4
+        System.out.println(9 % 2);          //1
+
+        System.out.println(4 / 2); //2
+        System.out.println(4 % 2);          //0
+
+        System.out.println(2 / 2);//1
+        System.out.println(2 % 2);          //0
+
+        System.out.println(1 / 2);  //0
+        System.out.println(1 % 2);          //1
+
+        int q1 = 1001111101; // Результат 637 из 10 в 2
+
+
+
+        // 2. Задание :
+
+        int w = 0b11100111;
+
+        System.out.println(1 * Math.pow(2,7) + 1 * Math.pow(2,6) + 1 * Math.pow(2,5) + 0 * Math.pow(2,4) + 0 * Math.pow(2,3) + 1 * Math.pow(2,2) + 1 * Math.pow(2,1) + 1 * Math.pow(2,0));
+
+        // 3. Задание:
+
+        int e = 637;
+
+        System.out.println(637 / 3); //212
+        System.out.println(637 % 3);        //1
+
+        System.out.println(212 / 3); //70
+        System.out.println(212 % 3);        //2
+
+        System.out.println(70 / 3); //23
+        System.out.println(30 % 3);         //0
+
+        System.out.println(23 / 3); //7
+        System.out.println(23 % 3);         //2
+
+        System.out.println(7 / 3);  //2
+        System.out.println(7 % 3);          //1
+
+        System.out.println(2 / 3); //0
+        System.out.println(2 % 3);          //2
+
+        int e1 = 212021;
+        
+    }
+}
